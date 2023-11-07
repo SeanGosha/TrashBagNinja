0022664a2da5cfb40f61d9fe1c843248d661eca7
Sean Gosha <sgosha.dev@gmail.com>
2023-11-07 11:13:57 -0600

    Finished highscore saving with PlayerPrefs

--- Assets/Scripts/FinalScore.cs
+++ Assets/Scripts/FinalScore.cs
@@ -10,9 +10,45 @@ public class FinalScore : MonoBehaviour
     //score counter script
     public ScoreCounter scoreCounter;
 
-    // Update is called once per frame
+    public int[] highScores;
+
     void Start()
     {
-        finalScoreText.text = ("SCORE: " + scoreCounter.playerScore.ToString());
+        finalScoreText.text = "SCORE: " + scoreCounter.playerScore.ToString();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            if (PlayerPrefs.HasKey("Highscore" + i))
+            {
+                if (scoreCounter.playerScore > PlayerPrefs.GetInt("Highscore" + i))
+                {
+                    // Store the current high score and replace it
+                    int temp = PlayerPrefs.GetInt("Highscore" + i);
+                    string date = System.DateTime.Now.ToString("MM/dd/yy");
+                    
+                    PlayerPrefs.SetInt("Highscore" + i, scoreCounter.playerScore);
+                    PlayerPrefs.SetString("HighscoreDate" + i, date);
+
+                    // Move lower scores down
+                    for (int j = i + 1; j <= 10; j++)
+                    {
+                        if (PlayerPrefs.HasKey("Highscore" + j))
+                        {
+                            int nextTemp = PlayerPrefs.GetInt("Highscore" + j);
+                            PlayerPrefs.SetInt("Highscore" + j, temp);
+                            temp = nextTemp;
+                        }
+                    }
+
+                    break; // Break the loop after updating the score
+                }
+            }
+            else
+            {
+                // If a slot is empty, just add the new score
+                PlayerPrefs.SetInt("Highscore" + i, scoreCounter.playerScore);
+                break; // Break the loop after adding the score
+            }
+        }
     }
 }
--- Assets/Scripts/HighscoreList.cs
+++ Assets/Scripts/HighscoreList.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HighscoreList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text[] highscoreTexts;
+
+    public TMP_Text[] highscoreDateTexts;
+
+    
+
+    public void Start()
     {
-        
+        SetHighScores();
+        Debug.Log(PlayerPrefs.GetInt("Highscore1"));
+        SetHighScoreTexts();
+        gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void SetHighScores()
     {
-        
+        for(int i = 1; i <= 10; i++)
+        {
+            if (PlayerPrefs.HasKey("Highscore" + i) == false)
+            {
+                PlayerPrefs.SetInt("Highscore" + i, 0);
+            }
+
+            if (PlayerPrefs.HasKey("HighscoreDate" + i) == false)
+            {
+                PlayerPrefs.SetString("HighscoreDate" + i, "00/00/00");
+            }
+        }
+    }
+
+    private void SetHighScoreTexts()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            highscoreTexts[i - 1].text = PlayerPrefs.GetInt("Highscore" + i).ToString();
+        }
+
+        for (int i = 1; i <= 10; i++)
+        {
+            highscoreDateTexts[i - 1].text = PlayerPrefs.GetString("HighscoreDate" + i);
+        }
     }
 }
